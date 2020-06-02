bca6b298837a3b0e0e3018863be8b4141bd93847
dfacko <dfacko@etfos.hr>
2020-06-02 14:06:51 +0100

    Finished web Api basic functions

---
+++ WebApp/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApp
+{
+	public class Osoba 
+	{ 
+		public string Name { get; set; }
+		public int Age { get; set; }
+		//public string prezime { get; set; }
+
+
+		public  Osoba(string name,int age)
+		{
+			this.Name = name;
+			this.Age = age;
+		}
+
+	}
+
+}
---
+++ WebApp/Controllers/AppController.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.DynamicData.ModelProviders;
+using System.Web.Http;
+using System.Web.UI.WebControls.WebParts;
+
+namespace WebApp.Controllers
+{
+    public class AppController : ApiController
+    {
+
+        static List<Osoba> popis = new List<Osoba>() { };
+        static List<string> strings = new List<string>() { "jedan", "dva", "tri" };
+
+
+
+        // GET: api/App
+            [HttpGet]
+            public IHttpActionResult Get()
+            {
+
+                if (popis.Count == 0)
+                {
+                    return NotFound();
+			    }
+			    else
+			    {
+                    return Ok(popis);
+			    }
+            }
+
+        
+            /* public List<Osoba> Get()
+            {
+
+            return popis;
+            }
+
+            public string Get(int id)
+            {
+                return "value";
+            }*/
+
+            [HttpPost]
+            public IHttpActionResult Post([FromBody] Osoba person)
+            {
+
+            popis.Add(person);
+            return Ok();
+            }
+
+            [HttpPost]
+            public HttpResponseMessage Post([FromUri]string name,int age)
+            {
+
+            strings.Add(name);
+
+            Osoba newPerson = new Osoba(name,age);
+            popis.Add(new Osoba(name,age)); 
+
+            return Request.CreateResponse(HttpStatusCode.OK);
+            }
+
+            [HttpPut]
+            public IHttpActionResult Put(string name, string newName, int newAge)
+            {
+                foreach(var person in popis)
+			    {
+                if (person.Name == name) {
+                    person.Name = newName;
+                    person.Age = newAge;
+                    return Ok();
+                }
+
+			    }
+                return NotFound();
+
+
+            }
+            
+            [HttpDelete]
+            public IHttpActionResult Delete([FromBody]string name)
+            {
+                foreach(var person in popis){
+                    if (person.Name == name) {
+                        popis.Remove(person);
+                        return Ok();
+                    }
+
+			    }
+                return BadRequest();
+                
+            }
+        
+    }
+}
+
+
